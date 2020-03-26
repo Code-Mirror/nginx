197732c66341475358802bf99923e2188babe905
RekGRpth <rekgrpth@gmail.com>
2020-03-26 15:10:02 +0500

    T_NGX_HTTP_UPSTREAM_RETRY_CC

--- src/http/ngx_http_core_module.c
+++ src/http/ngx_http_core_module.c
@@ -395,6 +395,15 @@ static ngx_command_t  ngx_http_core_commands[] = {
       offsetof(ngx_http_core_loc_conf_t, client_body_in_single_buffer),
       NULL },
 
+#if (T_NGX_HTTP_UPSTREAM_RETRY_CC)
+    { ngx_string("retry_cached_connection"),
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,
+      ngx_conf_set_flag_slot,
+      NGX_HTTP_LOC_CONF_OFFSET,
+      offsetof(ngx_http_core_loc_conf_t, retry_cached_connection),
+      NULL },
+#endif
+
     { ngx_string("sendfile"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_HTTP_LIF_CONF
                         |NGX_CONF_FLAG,
@@ -3417,6 +3426,9 @@ ngx_http_core_create_loc_conf(ngx_conf_t *cf)
     clcf->client_body_in_file_only = NGX_CONF_UNSET_UINT;
     clcf->subrequest_access_phase = NGX_CONF_UNSET;
     clcf->client_body_in_single_buffer = NGX_CONF_UNSET;
+#if (T_NGX_HTTP_UPSTREAM_RETRY_CC)
+    clcf->retry_cached_connection = NGX_CONF_UNSET;
+#endif
     clcf->internal = NGX_CONF_UNSET;
     clcf->sendfile = NGX_CONF_UNSET;
     clcf->sendfile_max_chunk = NGX_CONF_UNSET_SIZE;
@@ -3640,6 +3652,10 @@ ngx_http_core_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
                               NGX_HTTP_REQUEST_BODY_FILE_OFF);
     ngx_conf_merge_value(conf->client_body_in_single_buffer,
                               prev->client_body_in_single_buffer, 0);
+#if (T_NGX_HTTP_UPSTREAM_RETRY_CC)
+    ngx_conf_merge_value(conf->retry_cached_connection,
+                         prev->retry_cached_connection, 1);
+#endif
     ngx_conf_merge_value(conf->internal, prev->internal, 0);
     ngx_conf_merge_value(conf->sendfile, prev->sendfile, 0);
     ngx_conf_merge_size_value(conf->sendfile_max_chunk,
